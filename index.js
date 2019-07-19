2fccddbf4cfbe44b5b6d57c4cefadf1e5bdd56c8
lihefei <121131636@qq.com>
2019-07-19 17:34:23 +0800

    公用方法 gps常用方法 客户端检查 用户身份验证

---
+++ index.js
@@ -0,0 +1,11 @@
+import comnon from './lib/common.js';
+import gps from './lib/gps.js';
+import os from './lib/os.js';
+import userVerify from './lib/userVerify.js';
+
+export default {
+    comnon,
+    gps,
+    os,
+    userVerify
+};
